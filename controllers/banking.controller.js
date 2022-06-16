703038f6b1fa8f20bc5df2c8dfd1645122bb01bf
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-16 14:28:09 +0530

    Customer Module

--- controllers/banking.controller.js
+++ controllers/banking.controller.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken')
 
 const BankingCustomerModel = db.banking
 
-
 // Create
 exports.create = (req,res,next) =>{
     let {name,mobile,address,email,password} = req.body
@@ -29,27 +28,93 @@ exports.create = (req,res,next) =>{
 }
 // Get All
 exports.getAll = (req,res,next) =>{
+    BankingCustomerModel.find({},(err,result)=>{
+        if(err)
+        next(err)
+        res.json({
+            status:"success",
+            message:"All Bankers Are Here",
+            data:{
+                model:result
+            }
+        })
+    })
 
 }
 // Get By ID
 exports.getById = (req,res,next) =>{
+    BankingCustomerModel.findById(req.params.id,(err,result)=>{
+        if(err)
+        next(err)
+        res.json({
+            status:"Success",
+            message:"Successfully Retrieved by ID Banker",
+            data:{
+                model:result
+            }
+        })
+    })
 
 }
 // Get By Email
 exports.getByEmail = (req,res,next) =>{
+    BankingCustomerModel.find({"email":req.body.email},(err,result)=>{
+        if(err)
+        next(err)
+        res.json({
+            status:"Success",
+            message:"Successfully Retrieved",
+            data:{
+                model: result
+            }
+
+        })
+    })
 
 }
 // Update
 exports.updateById = (req,res,next) =>{
+BankingCustomerModel.findByIdAndUpdate(req.params.id,req.body,(err,result)=>{
+    if(err)
+    next(err)
+    res.json({
+        status:"Success",
+            message:"Successfully Updated Banker By ID",
+            data:{
+                model: result
+            }
 
+    })
+})
 }
 // Delete By Id
 exports.deleteById = (req,res,next) =>{
 
+    BankingCustomerModel.findByIdAndRemove(req.params.id,(err,result)=>{
+        if(err)
+        next(err)
+        res.json({
+            status:"Success",
+            message:"Successfully Deleted Banker By ID",
+            data:{
+                model: result
+            }
+
+        })
+    })
 }
 // Delete All
 exports.deleteAll = (req,res,next) =>{
+    BankingCustomerModel.remove({}, (err,result) => {
+        if(err)
+        next(err)
+        res.json({
+            status:"Success",
+            message:"Successfully Deleted Banker By ID",
+            data:{
+                model: result
+            }
 
+        })
+    })
 }
-
-
--- routes/banking.route.js
+++ routes/banking.route.js
@@ -6,5 +6,11 @@ const controller = require('../controllers/banking.controller')
 
 
 router.post('/banking/addCustomer', controller.create)
+router.get('/banking/getAll',controller.getAll)
+router.get('/banking/getById/:id',controller.getById)
+router.put('/banking/updateById/:id',controller.updateById)
+router.delete('/banking/deleteById/:id',controller.deleteById)
+router.delete('/banking/deleteAll',controller.deleteAll)
+router.post('/banking/getByEmail',controller.getByEmail)
 
 module.exports = router
